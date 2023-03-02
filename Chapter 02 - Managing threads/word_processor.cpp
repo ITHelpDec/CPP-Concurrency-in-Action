7c7b37ce601db584bbf82e925fadd8f387e7db1b
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-03-02 14:42:30 +0000

    Create word_processor.cpp

---
+++ Chapter 02 - Managing threads/word_processor.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <thread>
+
+// incomplete - will not compile
+void edit_document(const std::string &filename) {
+    open_document_and_display_gui(filename);
+    
+    while (!done_editing()) {
+        user_command cmd = get_user_input();
+        
+        if (cmd.type == open_new_document) {
+            std::string const new_name = get_filename_from_user();
+            std::thread t(edit_document.new_name);
+            t.detach();
+        } else {
+            process_user_input(cmd);
+        }
+    }
+}
