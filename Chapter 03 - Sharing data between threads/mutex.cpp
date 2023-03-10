0264fa3f8aa1d5ab00caa7e54a7c73c7944254d3
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-03-10 23:24:21 +0000

    Create mutex.cpp

---
+++ Chapter 03 - Sharing data between threads/mutex.cpp
@@ -0,0 +1,33 @@
+#include <list>
+#include <mutex>
+#include <algorithm>
+#include <random>
+#include <iostream>
+
+std::list<int> ilist;
+std::mutex mtx;
+
+int num() {
+    static thread_local std::default_random_engine e(std::random_device{}());
+    static std::uniform_int_distribution u(0, 10);
+    return u(e);
+}
+
+void add(int val) {
+    // std::lock_guard<std::mutex> guard(mtx);
+    std::lock_guard guard(mtx); // C++17-ism
+    ilist.emplace_back(val);
+}
+
+bool contains(int val) {
+    std::lock_guard guard(mtx);
+    return std::find(ilist.begin(), ilist.end(), val) != ilist.end();
+}
+
+int main()
+{
+    while (!contains(5)) { add(num()); }
+    std::cout << "Found 5!\n";
+    
+    return 0;
+}
