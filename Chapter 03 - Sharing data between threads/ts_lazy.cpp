8af9e828fd068750cdeffdf6c6da4a96eba2ba3a
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-04-06 18:39:02 +0300

    Create ts_lazy.cpp

---
+++ Chapter 03 - Sharing data between threads/ts_lazy.cpp
@@ -0,0 +1,38 @@
+#include <memory>
+#include <mutex>
+#include <iostream>
+
+std::shared_ptr<int> shared_int;
+std::mutex init_mutex;
+
+void init() {
+    std::unique_lock<std::mutex> lock(init_mutex);
+    
+    if (!shared_int) {
+        // shared_int.reset(new int(100));
+        
+        // opt for assignment over raw `new` call
+        shared_int = std::make_shared<int>(100);
+    }
+    
+    lock.unlock();
+}
+
+int main()
+{
+    std::cout << "before init(): " << shared_int.get() << " (uninitialised)\n";
+    
+    init();
+    
+    std::cout << "after init():  " << shared_int.get() << " (" << *shared_int.get() << ")\n";
+    
+    return 0;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// OUTPUT  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// before init(): 0x0 (uninitialised)
+// after init():  0x6000002054f8 (100)
+// Program ended with exit code: 0
