026ae48625044aaeef4a43907982dbc9ff6e4d0b
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-05-11 12:13:06 +0100

    ab1 -> `std::memory_order_relaxed`
    ab1 had relaxed orderings in the book, not acquire-release

--- Chapter 05 - The C++ memory model and operations on atomic types/acq_rel_same_thread.cpp
+++ Chapter 05 - The C++ memory model and operations on atomic types/acq_rel_same_thread.cpp
@@ -6,7 +6,7 @@ std::atomic<bool> ab1, ab2;
 std::atomic<int> ai;
 
 void write_ab1_then_ab2() {
-    ab1.store(true, std::memory_order_release);
+    ab1.store(true, std::memory_order_relaxed);
     ab2.store(true, std::memory_order_release);
 }
 
@@ -14,7 +14,7 @@ void read_ab2_then_ab1() {
     int counter = 0;
     
     while (!ab2.load(std::memory_order_acquire)) { ++counter; }
-    if (ab1.load(std::memory_order_acquire)) { ++ai; }
+    if (ab1.load(std::memory_order_relaxed)) { ++ai; }
     
     std::cout << "(read_ab2_then_ab1) counter = " << counter << '\n';
     std::cout << "(read_ab2_then_ab1)      ai = " << ai << '\n';
@@ -40,6 +40,6 @@ int main()
 //  OUTPUT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-// (read_ab2_then_ab1) counter = 829
+// (read_ab2_then_ab1) counter = 0
 // (read_ab2_then_ab1)      ai = 1
 // Program ended with exit code: 0
