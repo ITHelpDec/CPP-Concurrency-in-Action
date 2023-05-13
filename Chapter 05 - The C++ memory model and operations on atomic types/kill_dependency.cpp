0505430379ad8eb39305e4fbb1a7129769d26017
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-05-13 23:35:02 +0300

    Create kill_dependency.cpp

---
+++ Chapter 05 - The C++ memory model and operations on atomic types/kill_dependency.cpp
@@ -0,0 +1,31 @@
+#include <atomic>
+#include <iostream>
+#include <thread>
+
+int global_data[] = { 1, 2, 3, 4 };
+
+std::atomic<int> idx;
+
+void do_something_with(int i) {
+    std::cout << "(do_something_with) i = " << i << '\n';
+}
+
+void e() {
+    idx.store(2, std::memory_order_release);
+}
+
+void f() {
+    int i = idx.load(std::memory_order_consume);
+    do_something_with(global_data[std::kill_dependency(i)]);
+}
+
+int main()
+{
+    std::thread t1(e);
+    std::thread t2(f);
+    
+    t1.join();
+    t2.join();
+    
+    return 0;
+}
