b5c3cbb1c7514c0a7a57a1510f94af905b3e05fb
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-05-14 18:17:03 +0300

    Create fence.cpp

---
+++ Chapter 05 - The C++ memory model and operations on atomic types/fence.cpp
@@ -0,0 +1,45 @@
+#include <atomic>
+#include <thread>
+#include <iostream>
+
+std::atomic<bool> ab1, ab2;
+std::atomic<int> ai;
+
+void write_ab1_then_ab2() {
+    ab1.store(true, std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_release);
+    ab2.store(true, std::memory_order_relaxed);
+}
+
+void read_ab2_then_ab1() {
+    int counter = 0;
+    while (!ab2.load(std::memory_order_relaxed)) { ++counter; }
+    std::atomic_thread_fence(std::memory_order_acquire);
+    if (ab1.load(std::memory_order_relaxed)) { ++ai; }
+    
+    std::cout << "(read_ab2_then_ab1) counter = " << counter << '\n';
+}
+
+int main()
+{
+    ab1 = false;
+    ab2 = false;
+    ai = 0;
+    
+    std::thread t1(write_ab1_then_ab2);
+    std::thread t2(read_ab2_then_ab1);
+    
+    t1.join();
+    t2.join();
+    
+    assert(ai.load() != 0);
+    
+    return 0;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  OUTPUT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// (read_ab2_then_ab1) counter = 0
+// Program ended with exit code: 0
