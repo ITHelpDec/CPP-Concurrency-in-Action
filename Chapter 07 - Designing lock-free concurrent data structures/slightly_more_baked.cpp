610fbc48f6725d06abfbc14b4a1bc79e2f131dee
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-06-04 17:51:37 +0100

    Create slightly_more_baked.cpp

---
+++ Chapter 07 - Designing lock-free concurrent data structures/slightly_more_baked.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <future>
+
+namespace lf {
+template <typename T>
+class stack {
+public:
+    template <typename D>
+    void push(D &&data)
+    {
+        // counter_node_ptr new_node;
+        // new_node.ptr_ = new node(data);
+        // new_node.external_count_ = 0;
+        
+        // can be condensed into one line - "{}" or "= {}"
+        counted_node_ptr new_node{ new node(std::forward<D>(data)), 0 };
+        new_node.ptr_->next_ = head_.load();
+        while (!head_.compare_exchange_weak(new_node.ptr_->next_, new_node));
+    }
+    
+    std::shared_ptr<T> pop()
+    {
+        counted_node_ptr old_head = head_.load();
+        
+        for(;;) {
+            increase_head_count(old_head);
+            
+            node *ptr = old_head.ptr_;
+            
+            // if (!ptr) {
+            if (!ptr->next_.ptr_) {
+                // std::cout << "stack is empty!\n";
+                return std::shared_ptr<T>();
+            }
+            
+            if (head_.compare_exchange_strong(old_head, ptr->next_)) {
+                std::shared_ptr<T> result;
+                result.swap(old_head.ptr_->data_);
+        
+                int count_increase = old_head.external_count_ - 2;
+        
+                if (ptr->internal_count_.fetch_add(count_increase) == -count_increase)
+                {
+                    delete ptr;
+                }
+        
+                return result;
+            }
+        
+            else if (ptr->internal_count_.fetch_sub(1) == 1) { delete ptr; }
+        }
+    }
+    
+    // ~stack()
+    // {
+    //     std::cout << "destructor:\n";
+    //
+    //     int i = 0;
+    //     std::cout << "items popped...";
+    //
+    //     while(pop()) {
+    //         std::cout << ++i << "...";
+    //     }
+    //     std::cout << '\n';
+    // }
+    
+    ~stack() { while(pop()); }
+    
+private:
+    struct node;
+    struct counted_node_ptr {
+        node *ptr_;
+        int external_count_;
+    };
+    
+    struct node {
+        std::shared_ptr<T> data_;
+        counted_node_ptr next_;
+        
+        std::atomic<int> internal_count_;
+        
+        template <typename D>
+        node(D &&data) : data_(std::make_shared<T>(std::forward<D>(data))), internal_count_(0) { }
+    };
+    
+    std::atomic<counted_node_ptr> head_;
+    
+    void increase_head_count(counted_node_ptr& old_counter)
+    {
+        counted_node_ptr new_counter;
+        
+        do {
+            new_counter = old_counter;
+            ++new_counter.external_count_;
+        } while(!head_.compare_exchange_strong(old_counter, new_counter));
+        // MUST be a "do while", and not a "while"
+        
+        old_counter.external_count_ = new_counter.external_count_;
+    }
+};
+} // namespace lf (lock-free)
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+constexpr std::size_t num_threads = 10000;
+
+void fill(lf::stack<int> &stk) {
+    std::vector<std::future<void>> threads(num_threads);
+    
+    for (int i = 0; i != num_threads; ++i) {
+        threads[i] = std::async(std::launch::async, [&, i] () { stk.push(i); });
+    }
+    
+    for (auto &f : threads) { f.get(); }
+}
+
+std::shared_ptr<int> shared_clear(lf::stack<int> &stk) {
+    std::vector<std::future<std::shared_ptr<int>>> threads(num_threads);
+    
+    for (auto &f : threads) {
+        f = std::async(std::launch::async, [&] () { return stk.pop(); });
+    }
+    
+    std::shared_ptr<int> sp;
+    for (auto &f : threads) { sp = f.get(); }
+    
+    return sp;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+int main()
+{
+    lf::stack<int> stk;
+    
+    std::thread t1( [&] () { fill(stk); } );
+    std::thread t2( [&] () { fill(stk); } );
+    t1.detach();
+    t2.detach();
+    
+    std::shared_ptr<int> sp1, sp2;
+    auto f1 = std::async(std::launch::async, [&] () { return shared_clear(stk); });
+    auto f2 = std::async(std::launch::async, [&] () { return shared_clear(stk); });
+    sp1 = f1.get();
+    sp2 = f2.get();
+    
+    std::cout << "sp1: " << (sp1 ? *sp1 : -1) << '\n';
+    std::cout << "sp2: " << (sp2 ? *sp2 : -1) << '\n';
+    
+    return 0;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  OUTPUT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// sp1: 1
+// sp2: 33
+// Program ended with exit code: 0
