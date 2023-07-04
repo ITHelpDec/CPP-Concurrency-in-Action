c04c0bbfcdbb9756a959cc036c36b3428b51a646
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-07-04 07:33:44 +0100

    Update barrier.cpp

--- Chapter 08 - Designing concurrent code/barrier.cpp
+++ Chapter 08 - Designing concurrent code/barrier.cpp
@@ -4,22 +4,35 @@
 
 class barrier {
 public:
-    explicit barrier(std::size_t count) : count_(count), spaces_(count_), generation_(0) { }
+    explicit barrier(std::size_t count) : count_(count), generation_(0)
+    {
+        spaces_ = count_.load();
+    }
     
     void wait()
     {
         std::size_t my_gen = generation_;
         
         if (!--spaces_) {
-            spaces_ = count_;
+            spaces_ = count_.load();
             ++generation_;
         } else {
             while (generation_ == my_gen) { std::this_thread::yield(); }
         }
     }
     
+    void done_waiting()
+    {
+        --count_;
+        
+        if (!--spaces_) {
+            spaces_ = count_.load();
+            ++generation_;
+        }
+    }
+    
 private:
-    std::size_t count_;
+    std::atomic<std::size_t> count_;
     
     std::atomic<std::size_t> spaces_;
     std::atomic<std::size_t> generation_;
